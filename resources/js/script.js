866a620ba212fb6011d96cc97b93f2e3804fb106
github-mshkulepa <m.shkulepa@gmail.com>
2021-02-21 23:33:35 +0300

    Update script.js
    remove extra lines

--- resources/js/script.js
+++ resources/js/script.js
@@ -11,9 +11,7 @@ $(document).ready(function(){
     
 /* animations*/
     $('.js--wp-1').waypoint(function(direction) { 
-    
         $('.js--wp-1').addClass('animated fadeIn');
-         
     }, {
         offset:'50%'
     });
